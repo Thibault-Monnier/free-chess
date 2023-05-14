dca7e61feeccc813cfe9169304e7f4f0c0459ea5
Thibault Monnier <thibaultmonni@gmail.com>
2023-05-14 13:07:14 +0200

    Init tsc

---
+++ dist/main.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('Hello Worl 131654!');
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxPQUFPLENBQUMsR0FBRyxDQUFFLG9CQUFvQixDQUFDLENBQUMifQ==
