690248d261ef1f153ffdfadc4c286cf2960fb30b
Thibault Monnier <thibaultmonni@gmail.com>
2023-07-04 21:55:49 +0200

    Added moves and moveNb accessors

--- src/models/game.ts
+++ src/models/game.ts
@@ -4,44 +4,52 @@ import { PieceColor } from './types'
 
 export class Game {
     private startingBoard: Board = new Board()
-    private moves: Move[] = []
-    private moveNb: number = 0
+    private _moves: Move[] = []
+    private _moveNb: number = 0
+
+    get moves(): Move[] {
+        return this._moves
+    }
+
+    get moveNb(): number {
+        return this._moveNb
+    }
 
     get currentBoard(): Board {
-        if (this.moveNb > 0) {
-            return this.moves[this.moveNb - 1].endBoard
+        if (this._moveNb > 0) {
+            return this._moves[this._moveNb - 1].endBoard
         } else {
             return this.startingBoard
         }
     }
 
     get currentPlayerColor(): PieceColor {
-        return this.moveNb % 2 === 0 ? 'white' : 'black'
+        return this._moveNb % 2 === 0 ? 'white' : 'black'
     }
 
     get lastMove(): Move | undefined {
-        return this.moves[this.moveNb - 1]
+        return this._moves[this._moveNb - 1]
     }
 
     addMove(move: Move): void {
-        this.moves = this.moves.slice(0, this.moveNb)
-        this.moves.push(move)
-        this.moveNb++
+        this._moves = this._moves.slice(0, this._moveNb)
+        this._moves.push(move)
+        this._moveNb++
     }
 
     get canUndo(): boolean {
-        return this.moveNb > 0
+        return this._moveNb > 0
     }
 
     undo(): void {
-        if (this.canUndo) this.moveNb--
+        if (this.canUndo) this._moveNb--
     }
 
     get canRedo(): boolean {
-        return this.moveNb < this.moves.length
+        return this._moveNb < this._moves.length
     }
 
     redo(): void {
-        if (this.canRedo) this.moveNb++
+        if (this.canRedo) this._moveNb++
     }
 }
