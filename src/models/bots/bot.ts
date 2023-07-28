2d763085e52545df58f48f04abb9691fccd0b445
Thibault-Monnier <thibaultmonni@gmail.com>
2023-07-28 10:43:46 +0200

    Code Style

--- src/models/bots/bot.ts
+++ src/models/bots/bot.ts
@@ -2,7 +2,7 @@ import { Board } from '../board'
 import { Move } from '../move'
 
 export abstract class Bot {
-    constructor(public board: Board) {}
+    constructor(public board: Board, public depth: number | null) {}
 
     abstract run(): Move | null
 }
