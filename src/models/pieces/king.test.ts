7f0766979e819bd532c43cb9b0fa800dee4c3300
Thibault-Monnier <thibaultmonni@gmail.com>
2023-12-19 20:40:16 +0100

    Remove test.

--- src/models/pieces/king.test.ts
+++ src/models/pieces/king.test.ts
@@ -1,5 +1,4 @@
 import { Board } from '../board'
-import { Game } from '../game'
 import { AttackTable } from '../types'
 import { createEmptyAttackTable } from '../utils'
 
@@ -15,26 +14,6 @@ describe('possibleMoves', () => {
         // If castlings were not skipped from opponent (= white) moves computation, our implementation of
         // castlings would result in a "Maximum call stack size exceeded" error.
         expect(() => king.possibleMoves(kingSquareNb, board, createEmptyAttackTable())).not.toThrowError()
-    })
-
-    it('does not change the squares array size after castling', () => {
-        const FEN = '4k2r/7p/8/8/8/8/8/4K2R b Kk - 0 0'
-        const board = new Board(FEN)
-
-        const blackKingSquareNb = 60
-        const blackKing = board.squares[blackKingSquareNb]!
-
-        const blackKingMoves = blackKing.possibleMoves(blackKingSquareNb, board, createEmptyAttackTable())
-
-        const castlingMove = blackKingMoves.find((move) => {
-            const game = new Game(FEN)
-            game.addMove(move)
-            return game.calculateMoveNotation() === 'O-O'
-        })
-        expect(castlingMove).toBeDefined()
-        const boardAfterCastling = castlingMove!.endBoard
-
-        expect(boardAfterCastling.squares.length).toEqual(64)
     })
 })
 
