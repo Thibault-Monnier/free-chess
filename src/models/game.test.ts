1fed87b95d89d0b4b7accb3c6f2429f6d799f205
Thibault-Monnier <thibaultmonni@gmail.com>
2023-12-19 19:27:15 +0100

    Create calculateMoveNotation in Game

---
+++ src/models/game.test.ts
@@ -0,0 +1,80 @@
+import { Game } from './game'
+
+describe('check notation calculation', () => {
+    const notation = (FEN: string, startSquareNb: number, endSquareNb: number): string => {
+        const game = new Game(FEN)
+        const move = game.currentBoard.possibleMoves().find((move) => {
+            return move.startSquareNb === startSquareNb && move.endSquareNb === endSquareNb
+        })
+        game.addMove(move!)
+        return game.calculateMoveNotation()
+    }
+
+    describe('pawn moves', () => {
+        it('tests notation for a simple pawn move', () => {
+            expect(notation('k7/8/8/8/8/8/P6K/8 w - - 0 0', 8, 16)).toBe('a3')
+        })
+        it('tests notation for a simple pawn capture', () => {
+            expect(notation('k7/8/8/8/8/1p6/P6K/8 w - - 0 0', 8, 17)).toBe('axb3')
+        })
+        it('tests notation for a simple pawn en passant', () => {
+            expect(notation('k7/8/8/pP6/8/8/7K/8 w - a6 0 0', 33, 40)).toBe('bxa6')
+        })
+        it('tests notation for a simple pawn promotion', () => {
+            expect(notation('8/P6k/8/8/8/8/7K/8 w - - 0 0', 48, 56)).toBe('a8=Q')
+        })
+    })
+
+    describe('knight moves', () => {
+        it('tests notation for a simple knight move', () => {
+            expect(notation('k7/8/8/8/8/8/8/N6K w - - 0 0', 0, 10)).toBe('Nc2')
+        })
+        it('tests notation for a simple knight capture', () => {
+            expect(notation('k7/8/8/8/8/8/2r5/N6K w - - 0 0', 0, 10)).toBe('Nxc2')
+        })
+    })
+
+    describe('bishop moves', () => {
+        it('tests notation for a simple bishop capture', () => {
+            expect(notation('k7/8/8/8/8/8/1R5/b6K b - - 0 0', 0, 9)).toBe('Bxb2')
+        })
+    })
+
+    describe('rook moves', () => {
+        it('tests notation for a simple rook move', () => {
+            expect(notation('7k/8/8/8/8/8/8/R6K w - - 0 0', 0, 8)).toBe('Ra2')
+        })
+    })
+
+    describe('queen moves', () => {
+        it('tests notation for a simple queen move', () => {
+            expect(notation('7k/8/8/8/8/8/8/Q6K w - - 0 0', 0, 8)).toBe('Qa2')
+        })
+    })
+
+    describe('king moves', () => {
+        it('tests notation for a simple king move', () => {
+            expect(notation('k7/8/8/8/8/8/8/K7 w - - 0 0', 0, 8)).toBe('Ka2')
+        })
+    })
+
+    describe('ambiguous moves', () => {
+        describe('when several knights can move to the same square', () => {
+            it('tests when two knights are on the same rank', () => {
+                expect(notation('k7/8/8/8/8/8/K7/N1N5 w - - 0 0', 0, 17)).toBe('Nab3')
+            })
+
+            it('tests when two knights are on the same file', () => {
+                expect(notation('k7/8/8/8/8/N7/K7/N7 w - - 0 0', 0, 10)).toBe('N1c2')
+            })
+
+            it('tests when three knights are on the same file and rank', () => {
+                expect(notation('k7/8/8/8/8/N7/K7/N3N3 w - - 0 0', 0, 10)).toBe('Na1c2')
+            })
+
+            it('tests that coordinates are not added when they are not needed', () => {
+                expect(notation('k7/8/8/8/8/N7/K7/N3N3 w - - 0 0', 16, 10)).toBe('N3c2')
+            })
+        })
+    })
+})
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,10 +1,15 @@
 import { Board } from './board'
 import { Move } from './move'
+import { squareNbToCoordinates, squareNbToFileRank } from './utils'
 
 export class Game {
-    private startingBoard: Board = new Board()
+    private startingBoard: Board
     private _moves: Move[] = []
     private _moveNb: number = 0
+
+    constructor(FEN?: string) {
+        this.startingBoard = FEN ? new Board(FEN) : new Board()
+    }
 
     get moves(): Move[] {
         return this._moves
@@ -23,13 +28,19 @@ export class Game {
     }
 
     get lastMove(): Move | undefined {
-        return this._moves[this._moveNb - 1]
+        return this._moves[this.getLastMoveIndex()]
     }
 
     addMove(move: Move): void {
         this._moves = this._moves.slice(0, this._moveNb)
         this._moves.push(move)
         this._moveNb++
+
+        console.log(this.calculateMoveNotation())
+    }
+
+    private getLastMoveIndex(): number {
+        return this.moves.length - 1
     }
 
     jumpToMove(moveNb: number) {
@@ -50,5 +61,86 @@ export class Game {
 
     redo(): void {
         if (this.canRedo) this._moveNb++
+    }
+
+    public calculateMoveNotation(moveIndex = this.getLastMoveIndex()): string {
+        let notation: string = ''
+
+        const move = this._moves[moveIndex]
+        const piece = move.piece
+        const endBoard = move.endBoard
+
+        // Add the piece symbol / pawn file
+        if (move.type === 'shortCastle') {
+            notation += 'O-O'
+        } else if (move.type === 'longCastle') {
+            notation += 'O-O-O'
+        } else {
+            if (piece.name === 'pawn' && (move.type === 'capture' || move.type === 'capturePromotion')) {
+                // The pawn file is added only if it is a capture
+                notation += squareNbToCoordinates(move.startSquareNb)[0]
+            } else {
+                notation += piece.notationChar
+            }
+        }
+
+        // Add the start square coordinates if there are ambiguities
+        // There can't be ambiguities for pawns because in case of a capture the column is already specified
+        if (piece.name !== 'pawn') {
+            const ambiguousStartSquareNbs: number[] = []
+            const boardBeforeMove = moveIndex > 0 ? this._moves[moveIndex - 1].endBoard : this.startingBoard
+            boardBeforeMove.possibleMoves().forEach((testedMove) => {
+                if (
+                    testedMove.piece.name === piece.name &&
+                    testedMove.endSquareNb === move.endSquareNb &&
+                    testedMove.startSquareNb !== move.startSquareNb
+                ) {
+                    ambiguousStartSquareNbs.push(testedMove.startSquareNb)
+                }
+            })
+
+            if (ambiguousStartSquareNbs.length > 0) {
+                const files = ambiguousStartSquareNbs.map((squareNb) => squareNbToFileRank(squareNb).file)
+                const ranks = ambiguousStartSquareNbs.map((squareNb) => squareNbToFileRank(squareNb).rank)
+
+                const { file, rank } = squareNbToFileRank(move.startSquareNb)
+                const coordinates = squareNbToCoordinates(move.startSquareNb)
+
+                if (!files.some((theFile) => file === theFile)) {
+                    notation += coordinates[0]
+                } else if (!ranks.some((theRank) => rank === theRank)) {
+                    notation += coordinates[1]
+                } else {
+                    notation += coordinates
+                }
+            }
+        }
+
+        // Add the capture symbol if it is a capture
+        if (move.type === 'capture' || move.type === 'capturePromotion') {
+            notation += 'x'
+        }
+
+        // Add the end square coordinates
+        if (move.type !== 'shortCastle' && move.type !== 'longCastle') {
+            const endSquareCoordinates = squareNbToCoordinates(move.endSquareNb)
+            notation += endSquareCoordinates
+        }
+
+        // Add the promotion symbol if it is a promotion
+        if (move.type === 'promotion' || move.type === 'capturePromotion') {
+            notation += '=' + endBoard.squares[move.endSquareNb]!.notationChar
+        }
+
+        // Add the check / checkmate / stalemate symbol
+        if (endBoard.endOfGame === 'checkmate') {
+            notation += '#'
+        } else if (endBoard.isInCheck()) {
+            notation += '+'
+        } else if (endBoard.endOfGame === 'stalemate') {
+            notation += '½'
+        }
+
+        return notation
     }
 }
