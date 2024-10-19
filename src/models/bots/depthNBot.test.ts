76d5bf70cd232b00b11edcb798b19a4ef488ec54
Thibault Monnier <thibaultmonni@gmail.com>
2024-10-19 20:49:47 +0200

    Remove console.log in a test

--- src/models/bots/depthNBot.test.ts
+++ src/models/bots/depthNBot.test.ts
@@ -6,7 +6,6 @@ describe('PieceSquareTableEvaluator', () => {
     it('tests that the bot detects checkmate', () => {
         const board = new Board('8/8/8/8/8/k7/7r/K7 b - - 0 0')
         const bestMove = new Move(board.squares[15]!, 15, 7, new Board('8/8/8/8/8/k7/8/K6r w - - 0 1'), 'normal')
-        console.log(bestMove)
         expect(new DepthNBot(board, 2).run()?.move).toEqual(bestMove)
     })
 })
